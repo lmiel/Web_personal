d6c71d7cdd03fb8ca4c592eced6b0891d01de790
lmiel <lucia.mielgo@gmail.com>
2024-11-15 19:30:30 +0100

    not pretty yer

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -16,6 +16,8 @@ LANGUAGE_CODE = 'es'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATICFILES_DIRS = [BASE_DIR / "core/static"]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
