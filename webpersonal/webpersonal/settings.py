d0cce698e0844349cae79def55fc710cee38bfae
lmiel <lucia.mielgo@gmail.com>
2024-11-23 14:42:50 +0100

    almost finished

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 LANGUAGE_CODE = 'es'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,3 +126,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
