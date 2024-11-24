e97715d56e1ffb834ded4730053c204d3f0764af
LUCIA MIELGO TORRES <114688227+lmiel@users.noreply.github.com>
2024-11-24 12:05:14 +0100

    Update build_files.sh

--- webpersonal/build_files.sh
+++ webpersonal/build_files.sh
@@ -1,2 +1,4 @@
 pip install -r requirements.txt
-python3.11 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
+mkdir -p .vercel/output/static
+cp -r staticfiles/ .vercel/output/static/
